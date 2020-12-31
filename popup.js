a222f08eb1c1d0101b8896c693f8be5ca57c9ff4
pizonka662 <pizonka662@gmail.com>
2020-12-30 21:35:41 -0600

    add popup.js

---
+++ popup.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+	if(request.topic == "pageText") {
+		message.innerText = request.source;
+	}
+});
+
+function onWindowLoad() {
+	var message = document.querySelector('#message');
+	chrome.tabs.executeScript(null, {
+		file: "getPageText.js"
+	}, function() {
+		if (chrome.runtime.lastError) {
+			message.innerText = "Error! Problem injecting script: \n" + chrome.runtime.lastError.message;
+		}
+	});
+}
+
+window.onload = onWindowLoad; 
