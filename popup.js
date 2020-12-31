9423310b19512aefb05df1573cde4ac5201f74f1
pizonka662 <pizonka662@gmail.com>
2020-12-30 23:09:42 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,6 +2,15 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 	if(request.topic == "pageText") {
 		var result = analyze(request.text)
 		document.getElementById("score").innerHTML = result.sum
+		if(result.sum > 0) {
+			document.getElementById("score").style.color = "Green"
+		}
+		else if( result.sum < 0) {
+			document.getElementById("score").style.color = "Red"
+		}
+		else {
+			document.getElementById("socre").style.color = "DimGrey"
+		}
 		document.getElementById("words").innerHTML = result.words + " words"
 		document.getElementById("average").innerHTML = Math.round(result.sum / result.words * 1000) / 1000
 	}
